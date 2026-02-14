3829930bbc661915ed3ebc05d3c8babfa6425950
unknown <adammastera8@gmail.com>
2026-02-14 19:47:40 +0100

    Zlib function and main created

---
+++ functions/zlib.lua
@@ -0,0 +1,2 @@
+local zlib = loadstring(game:HttpGet("https://github.com/adamMasMusic/asseter/raw/refs/heads/main/utils/zlib.lua"))()
+_G.assetStealer.zlib = zlib
---
+++ main.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://github.com/adamMasMusic/asseter/raw/refs/heads/main/functions/windUI.lua"))
+repeat task.wait() until _G.assetStealer
+
+local assetStealer = _G.assetStealer
